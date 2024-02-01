b50a383e3003f15975d210b41503629c7eb3142f
hamza <hmzaitahmad@gmail.com>
2024-02-01 22:14:10 +0100

    ajouter elevatedButton

--- frontend/lib/HomePage.dart
+++ frontend/lib/HomePage.dart
@@ -12,7 +12,75 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Stack(),
+        child: Stack(
+          children: [
+            Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(colors: [
+                    Colors.orangeAccent,
+                    Colors.limeAccent
+                  ])),
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(vertical: 100,horizontal: 30),
+              child: Material(
+                elevation: 10,
+                borderRadius: BorderRadius.circular(30),
+                child: Container(
+                  height: MediaQuery.of(context).size.height,
+                  width: MediaQuery.of(context).size.width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white24
+                  ),
+                ),
+              ),
+            ),
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ElevatedButton.icon(
+                      onPressed: (){},
+                      icon: const Icon(Icons.create),
+                      label:  const Text('Create'),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent
+                      )),
+                       const Padding(padding: EdgeInsets.only(top: 20)),
+                  ElevatedButton.icon(
+                      onPressed: (){},
+                      icon: const Icon(Icons.update),
+                      label: const Text('Update'),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent
+                      )
+                  ),
+                  const  Padding(padding: EdgeInsets.only(top: 20)),
+                  ElevatedButton.icon(
+
+                      onPressed: (){},
+                      icon:  const Icon(Icons.delete),
+                      label:  const Text('Delete'),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent
+                      )
+                  ),
+                  const  Padding(padding: EdgeInsets.only(top: 20)),
+                  ElevatedButton.icon(
+                      onPressed: (){},
+                      icon: const Icon(Icons.read_more_rounded),
+                      label: const Text('Read'),
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.orangeAccent
+                      )
+                  ),
+
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
